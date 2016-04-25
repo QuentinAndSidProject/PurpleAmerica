e29b85e68e7e7884578ae8ab909ec5b7af90302c
hcps-davidsoqs <hcps-davidsoqs@180-18DAVIDSOQS.hcps.internal>
2016-04-25 14:48:15 -0400

    I added the functionality to read the file USA.txt which can be expanded to include the rest of the states as well as start on reading the txt files for drawing the states.

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -12,13 +12,23 @@
  */
 package map;
 import edu.princeton.cs.introcs.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 /**
  *
  * @author 
  */
 public class PoliticalMap {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        StdDraw.setCanvasSize(700,700);
         
+        File[] fileAry = new File[51];
+        fileAry[0] = new File(PoliticalMap.class.getResource("/data/USA.txt").toURI());
         
+        for(int i = 0; i < 1; i++){
+            StateDraw.drawState(fileAry[i]);
+            
+        }
     }
 }
