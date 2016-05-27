e39613d3edf127b7a75e9e6d476cace933b54bf7
hcps-davidsoqs <hcps-davidsoqs@180-18DAVIDSOQS.hcps.internal>
2016-05-27 15:21:11 -0400

    Done with main part of program, now we just need to scale the rest of the states.

--- src/map/StateDraw.java
+++ src/map/StateDraw.java
@@ -14,8 +14,7 @@ import java.io.*;
  * @author hcps-davidsoqs
  */
 public class StateDraw {
-    public static void drawState(File stateName, File year) throws Exception{
-        StdDraw.show(0);
+    public static void drawState(File stateName, File year, int byState, String state) throws Exception{
         int numCounties;
         int numPoints;
         double[] pointAryX;
@@ -45,20 +44,239 @@ public class StateDraw {
             pointAryX = new double[numPoints];
             pointAryY = new double[numPoints];
             
-            for(int i = 0; i < numPoints; i++){
-                pointAryX[i] = (((scan.nextDouble() + 124.731216)/ 60) + .03);
-                pointAryY[i] = (((scan.nextDouble() - 24.544102)/ 30) + .03);
+            if(byState == 1){
+                for(int i = 0; i < numPoints; i++){
+                    pointAryX[i] = (((scan.nextDouble() + 124.731216)/ 60) + .03);
+                    pointAryY[i] = (((scan.nextDouble() - 24.544102)/ 30) + .03);
+                }
+            }
+            else{
+                double pointX;
+                double pointY;
+                for(int i = 0; i < numPoints; i++){
+                    pointX = scan.nextDouble();
+                    pointY = scan.nextDouble();
+                    
+                    if(state.toUpperCase().equals("AK")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) - .2);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .2);
+                    }
+                    else if(state.toUpperCase().equals("AL")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 16) - 1.9);
+                        pointAryY[i] = (((pointY - 24.544102)/ 8) - .5);
+                    }
+                    else if(state.toUpperCase().equals("AR")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 10) - 2.7);
+                        pointAryY[i] = (((pointY - 24.544102)/ 5) - 1.5);
+                    }
+                    else if(state.toUpperCase().equals("AZ")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 16) - .4);
+                        pointAryY[i] = (((pointY - 24.544102)/ 8) - .7);
+                    }
+                    else if(state.toUpperCase().equals("CA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 24) + .3);
+                        pointAryY[i] = (((pointY - 24.544102)/ 12) - .5);
+                    }
+                    else if(state.toUpperCase().equals("CO")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 12) - 1.1);
+                        pointAryY[i] = (((pointY - 24.544102)/ 6) - 1.85);
+                    }
+                    else if(state.toUpperCase().equals("CT")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 12) - 3.8);
+                        pointAryY[i] = (((pointY - 24.544102)/ 6) - 2.2);
+                    }
+                    else if(state.toUpperCase().equals("DC")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 10) - 4.3);
+                        pointAryY[i] = (((pointY - 24.544102)/ 5) - 2.1);
+                    }
+                    else if(state.toUpperCase().equals("DE")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("FL")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("GA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("HI")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("IA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("ID")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("IL")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("IN")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("KS")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("KY")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("LA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MD")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("ME")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MI")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MN")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MO")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MS")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("MT")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NC")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("ND")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NE")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NH")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NJ")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NM")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NV")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("NY")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("OH")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("OK")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("OR")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("PA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("RI")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("SC")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("SD")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("TN")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("TX")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("USA")){
+                        pointAryX[i] = (((scan.nextDouble() + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((scan.nextDouble() - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("UT")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("VA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 14) - 2.7);
+                        pointAryY[i] = (((pointY - 24.544102)/ 7) - 1.3);
+                    }
+                    else if(state.toUpperCase().equals("VT")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("WA")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("WI")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("WV")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                    else if(state.toUpperCase().equals("WY")){
+                        pointAryX[i] = (((pointX + 124.731216)/ 60) + .03);
+                        pointAryY[i] = (((pointY - 24.544102)/ 30) + .03);
+                    }
+                }
             }
             
             if(!nextCounty.equals(lastCounty)){
                 pollScan.nextLine();
                 String poll = pollScan.next();
-                System.out.println(poll);
+                //System.out.println(poll);
                 while(poll.length() < 10){
                     poll = pollScan.next();
                 }
                 poll = poll.replaceAll(",", " ");       
-                
+
 
                 Scanner stringScan = new Scanner(poll);
                 stringScan.next();
@@ -75,8 +293,6 @@ public class StateDraw {
             StdDraw.filledPolygon(pointAryX, pointAryY);
             StdDraw.setPenColor(Color.BLACK);
             StdDraw.polygon(pointAryX, pointAryY);
-            
         }
-        StdDraw.show(0);
     }
 }
